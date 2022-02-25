7cfb2faa858bfa1e07a6cc418778c92b1666358e
zbrose <zrbrose@gmail.com>
2022-02-25 14:43:50 -0800

    auth complete

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,73 @@
 const express = require('express')
 const db = require('../models')
+const user = require('../models/user')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const cryptojs = require('crypto-js')
+require('dotenv').config()
+
+
+router.get('/profile',(req,res)=>{
+    res.render('users/profile.ejs')
+})
 
 router.get('/new',(req,res)=>{
     res.render('users/new.ejs')
 })
 
-module.exports = router
+router.post('/', async (req,res)=>{
+   const [newUser, created] = await db.user.findOrCreate({
+        where: {email: req.body.email}
+    })
+    if(!created){
+        console.log("user already exists")
+        //render login page and send appropriate message
+    } else {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+        newUser.password = hashedPassword
+        await newUser.save()
+
+        // encrypt the user id via AES
+        const encryptedUserId = cryptojs.AES.encrypt(newUser.id.toString(),process.env.SECRET)
+        const encryptedUserIdString = encryptedUserId.toString()
+        // store the encpted id in the cookie of the res obj
+        res.cookie('userId', encryptedUserIdString)
+        // redirect back to the home page
+        res.redirect('/')
+    }
+})
+
+router.get('/login',(req,res)=>{
+    res.render('users/login.ejs', {error: null})
+})
+
+router.post('/login', async (req,res)=>{
+   const user = await db.user.findOne({
+       where: {email: req.body.email}
+   })
+   if(!user){ //didnt find usr in database
+       console.log('user not found')
+       res.render('users/login.jes',{error: 'Invalid email/password'})
+   } else if (!bcrypt.compareSync(req.body.password, user.password)) {
+       console.log('incorrect password')
+       res.render('users/login.ejs',{error: 'Invalid email/password'})
+   } else {
+       console.log('logging in the user')
+          // encrypt the user id via AES
+          const encryptedUserId = cryptojs.AES.encrypt(user.id.toString(),process.env.SECRET)
+
+          const encryptedUserIdString = encryptedUserId.toString()
+          // store the encpted id in the cookie of the res obj
+          res.cookie('userId', encryptedUserIdString)
+          // redirect back to the home page
+          res.redirect('/')
+   }
+})
+
+router.get('/logout',(req,res)=>{
+    console.log('loggin out')
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
+module.exports = router 
--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express') // import express
 const app = express() // create an express instance
 const ejsLayouts = require('express-ejs-layouts') // import ejs layouts
 require('dotenv').config()// allows us to access env vars
+const cookieParser = require('cookie-parser')
+const cryptojs = require('crypto-js')
+const db = require('./models/index.js')
+
 
 //MIDDLEWARE
 app.set('view engine', 'ejs') // set the view engine to ejs
 app.use(ejsLayouts) // tell express we want to use layouts
+app.use(cookieParser()) //give us acces to req.cookies
+app.use(express.urlencoded({extended:false})) //body parser to make req.body work
+
+//CUSTOM LOGIN MIDDLEWARE
+app.use(async (req,res,next)=>{
+    if (req.cookies.userId){
+        const decryptedId = cryptojs.AES.decrypt(req.cookies.userId, process.env.SECRET)
+        const decryptedIdString = decryptedId.toString(cryptojs.enc.Utf8)
+        const user = await db.user.findByPk(decryptedIdString)
+        res.locals.user = user
+    } else res.locals.user = null
+    next()
+})
 
 //CONTROLLERS
 app.use('/users',require('./controllers/users.js'))
